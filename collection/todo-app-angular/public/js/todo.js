87256ca4db01e33ace2b8a544daaae45652c4128
Franco Alarcón <franco.alarcon@gmail.com>
2014-07-16 16:47:01 -0300

    added login to the todo-app-angular

--- collection/todo-app-angular/public/js/todo.js
+++ collection/todo-app-angular/public/js/todo.js
@@ -1,67 +1,166 @@
 var todoApp = angular.module('todoApp', []);
 
-todoApp.controller('TodoController', function($scope, $http) {
+todoApp.config(function($routeProvider) {
+  $routeProvider
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'LoginController',
+        })
+        .when('/todos', {
+            templateUrl: 'todo-list.html',
+            controller: 'TodoController',
+            requireLogin: true
+        })
+        .otherwise({
+            redirectTo: '/login'
+        });
+});
 
-  $scope.todos = [];
-
-  // Get all todos
-  $http.get('/todos')
-    .success(function(todos) {
-      $scope.loaded = true;
-      $scope.todos = todos;
-    }).error(function(err) {
-      alert(err);
+todoApp.run(function($rootScope, $state, $location, UserService) {
+    $rootScope.$on('$stateChangeStart', function(event, next, current) {
+        if (current.requireLogin && !UserService.isAuthenticated()) {
+            $location.path('/login');
+        }
+        
     });
+});
 
-  $scope.addTodo = function(title) {
-    $http.post('/todos', {
-      title: title
-    }).success(function(todo) {
-      $scope.newTodoTitle = '';
-      $scope.todos.push(todo);
-    }).error(function(err) {
-      // Alert if there's an error
-      return alert(err.message || "an error occurred");
-    });
-  };
+todoApp.service('UserService', function($q, $http) {
+    var _user = undefined;
+    
+    return {
+        login: function(user) {
+            var d = $q.defer();
+            
+            $http.post('/users/login', user)
+                .success(function(resp) {
+                    _user = resp;
+                    d.resolve(_user);
+                })
+                .error(function(err) {
+                    console.log(err);
+                    d.reject(err);
+                });
+            
+            return d.promise;
+        },
+        logout: function() {
+            var d = $q.defer();
+            _user = undefined;
+            
+            $http.post('/users/login')
+                .success(function(resp) {
+                    console.log(resp);
+                    d.resolve();
+                })
+                .error(function(err) {
+                    console.log(err);
+                    d.reject(err);
+                });
 
-  $scope.changeCompleted = function(todo) {
-    // Update the todo
-    $http.put('/todos/' + todo.id, {
-      completed: todo.completed
-    }).error(function(err) {
-      return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
-    });
-  };
+            return d.promise;
+        },
+        getUser: function() {
+            return _user;
+        },
+        isAuthenticated: function() {
+            return Boolean(_user);
+        }
+    };
+});
 
-  $scope.removeCompletedItems = function() {
-    $http.get('/todos', {
-      params: {
-        completed: true
-      }
-    }).success(function(todos) {
-      todos.forEach(function(t) { deleteTodo(t); });
-    });
-  };
+todoApp.controller('LoginController', function(UserService, $location, $scope) {
+    $scope.user = {};
+    
+    $scope.login = function(user) {
+        UserService.login(user).then(
+            function(user) {
+                $location.path('/todos');
+            }, 
+            function(err) {
+                alert(err.message);
+            }
+        );
+    };
+});
 
-  function deleteTodo(todo) {
-    $http.delete('/todos/' + todo.id, {
-      params: {
-        completed: true
-      }
-    }).success(function() {
-      // Find the index of an object with a matching id
-      var index = $scope.todos.indexOf(
-          $scope.todos.filter(function(t) {
-            return t.id === todo.id;
-          })[0]);
+todoApp.controller('TodoController', function($scope, $http, $location, UserService) {
+    /*
+    if (!UserService.isAuthenticated()) {
+        $location.path('/login');
+    }
+    */
+    $scope.todos = [];
 
-      if (index !== -1) {
-        $scope.todos.splice(index, 1);
-      }
-    }).error(function(err) {
-      alert(err.message || "an error occurred");
-    });
-  }
+    // Get all todos
+    $http.get('/todos')
+        .success(function(todos) {
+            $scope.loaded = true;
+            $scope.todos = todos;
+        }).error(function(err) {
+            alert(err);
+        });
 
+    $scope.logout = function() {
+        console.log("Logout");
+        UserService.logout().then(
+            function() {
+                $location.path('/login');
+            },
+            function() {
+                $location.path('/login');
+            }
+        );        
+    };
+    
+    $scope.addTodo = function(title) {
+        $http.post('/todos', {
+            title: title
+        }).success(function(todo) {
+            $scope.newTodoTitle = '';
+            $scope.todos.push(todo);
+        }).error(function(err) {
+            // Alert if there's an error
+            return alert(err.message || "an error occurred");
+        });
+    };
+
+    $scope.changeCompleted = function(todo) {
+        // Update the todo
+        $http.put('/todos/' + todo.id, {
+            completed: todo.completed
+        }).error(function(err) {
+            return alert(err.message || (err.errors && err.errors.completed) || "an error occurred");
+        });
+    };
+
+    $scope.removeCompletedItems = function() {
+        $http.get('/todos', {
+                params: {
+                    completed: true
+                }
+            }).success(function(todos) {
+                todos.forEach(function(t) { deleteTodo(t); });
+            });
+    };
+
+    function deleteTodo(todo) {
+        $http.delete('/todos/' + todo.id, {
+            params: {
+                completed: true
+            }
+        }).success(function() {
+            // Find the index of an object with a matching id
+            var index = $scope.todos.indexOf(
+                $scope.todos.filter(function(t) {
+                return t.id === todo.id;
+            })[0]);
+
+            if (index !== -1) {
+                $scope.todos.splice(index, 1);
+            }
+        }).error(function(err) {
+            alert(err.message || "an error occurred");
+        });
+    }
 });
